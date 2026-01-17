9fefd276d5720f201a618201b9ed596146bf6be9
Rainy Sharma <raiinysharmaa.20@gmail.com>
2026-01-17 19:05:19 +0530

    deployment ready

--- src/components/BuyActionWindow.js
+++ src/components/BuyActionWindow.js
@@ -6,13 +6,14 @@ import axios from "axios";
 import GeneralContext from "./GeneralContext";
 
 import "./BuyActionWindow.css";
+import { API_BASE_URL } from "../config";
 
 const BuyActionWindow = ({ uid }) => {
   const [stockQuantity, setStockQuantity] = useState(1);
   const [stockPrice, setStockPrice] = useState(0.0);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post(`${API_BASE_URL}/newOrder`, {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
